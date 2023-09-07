515b002c2f5d722710588609cd0245c194165366
Olreliable-DDM <diego@dimo.zone>
2023-09-07 16:09:49 -0300

    Updated MarketplaceTokenIssuer with new features/fixes

--- MarketplaceTokenIssuer.sol
+++ MarketplaceTokenIssuer.sol
@@ -2,96 +2,97 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 /**
  * @title MarketplaceTokenIssuer
- * @dev This smart contract enables authorized partners (represented as NFTs) to issue tokens to users.
+ * @dev This smart contract allows authorized entities (represented as NFTs) to issue ERC20 tokens to users.
+ * This version of the contract is upgradable. To upgrade, follow the upgrade guide.
  */
-contract MarketplaceTokenIssuer is ReentrancyGuard, Ownable {
-    IERC20 private _token; // ERC20 token to be issued
-    IERC721 public partnerNFT; // NFT representing authorized partners
-    address public masterWallet; // Address where all the partner NFTs are stored
+contract MarketplaceTokenIssuer is Initializable, AccessControl {
+    // Define roles for role-based access control
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+    
+    // ERC20 token and NFT interfaces
+    IERC20 private _token; 
+    IERC721 public partnerNFT;
 
-    uint256 public constant MIN_ISSUANCE_AMOUNT = 0;
-    uint256 public constant COOLDOWN_PERIOD = 4 weeks; // Cooldown period for token issuance
+    // The master wallet that is allowed to issue tokens
+    address public masterWallet;
 
-    mapping(address => mapping(uint256 => uint256)) public lastIssuanceDate; // Last issuance dates for user-token pairs
+    // Cooldown period between issuance (in seconds)
+    uint256 public constant COOLDOWN_PERIOD = 28 days;
 
+    // Mapping to keep track of the last issuance date for each user-token pair
+    mapping(address => mapping(uint256 => uint256)) public lastIssuanceDate;
+
+    /// @notice Emitted when tokens are successfully issued
     event TokensIssued(address indexed user, uint256 indexed tokenId, uint256 amount);
+    
+    /// @notice Emitted when a partner NFT address is set
     event PartnerNFTSet(address partnerNFTAddress);
-    event TokensDeposited(uint256 amount);
-
-    constructor(address tokenAddress, address _masterWallet) {
+    
+    /// @dev Initializes the contract setting the token and masterWallet addresses
+    /// @param tokenAddress The address of the ERC20 token
+    /// @param _masterWallet The address allowed to issue tokens
+    function initialize(address tokenAddress, address _masterWallet) public initializer {
         _token = IERC20(tokenAddress);
         masterWallet = _masterWallet;
-    }
-
-    modifier cooldownPassed(address user, uint256 tokenId) {
-        require(block.timestamp >= lastIssuanceDate[user][tokenId] + COOLDOWN_PERIOD, "ERR01: Cooldown period not passed");
-        _;
+        
+        // Setup default roles
+        _setupRole(OWNER_ROLE, _msgSender());
     }
 
     /**
-     * @dev Sets the partner NFT address. Can only be called by the contract owner.
-     * @param nftAddress - Address of the NFT representing partners.
+     * @dev Checks if the cooldown has passed for a specific user-token pair.
+     * @param user The user's address.
+     * @param tokenId The NFT token ID.
+     * @return True if the cooldown has passed, false otherwise.
      */
-    function setPartnerNFT(address nftAddress) external onlyOwner {
+    function isCooldownPassed(address user, uint256 tokenId) public view returns (bool) {
+        return block.timestamp >= lastIssuanceDate[user][tokenId] + COOLDOWN_PERIOD;
+    }
+
+    /**
+     * @dev Sets the partner NFT address.
+     * @param nftAddress The address of the partner NFT.
+     */
+    function setPartnerNFT(address nftAddress) external {
+        require(hasRole(OWNER_ROLE, _msgSender()), "Caller is not the owner");
         partnerNFT = IERC721(nftAddress);
         emit PartnerNFTSet(nftAddress);
     }
 
     /**
-     * @dev Partners can issue tokens to multiple users.
-     * @param users - Array of users to issue tokens to.
-     * @param tokenIds - Array of tokenIds representing partners.
-     * @param amounts - Array of token amounts to issue.
+    * @dev Returns the number of tokens available in the contract.
+    * @return The number of tokens available.
+    */
+    function availableTokens() public view returns (uint256) {
+        return _token.balanceOf(address(this));
+    }
+
+
+    /**
+     * @dev Issue tokens to multiple users.
+     * @param users Array of user addresses.
+     * @param tokenIds Array of NFT token IDs.
+     * @param amounts Array of token amounts.
      */
     function issueTokensToMultipleUsers(
-        address[] memory users,
-        uint256[] memory tokenIds,
+        address[] memory users, 
+        uint256[] memory tokenIds, 
         uint256[] memory amounts
-    )
-        external
-        nonReentrant
-    {
-        require(users.length == tokenIds.length && users.length == amounts.length, "ERR02: Array lengths mismatch");
-
-        uint256 totalAmount = 0;
-        for (uint256 i = 0; i < users.length; i++) {
-            require(amounts[i] >= MIN_ISSUANCE_AMOUNT, "ERR03: Amount below the minimum issuance threshold");
-            require(partnerNFT.ownerOf(tokenIds[i]) == masterWallet, "ERR04: Not authorized for this partner NFT");
-            cooldownPassed(users[i], tokenIds[i]); // Apply cooldown check for user-tokenId pair
-            totalAmount += amounts[i];
-        }
-
-        require(_token.balanceOf(address(this)) >= totalAmount, "ERR05: Not enough token balance in the contract");
+    ) external {
+        require(hasRole(OWNER_ROLE, _msgSender()), "Caller is not the owner");
+        require(users.length == tokenIds.length && users.length == amounts.length, "Array lengths mismatch");
 
         for (uint256 i = 0; i < users.length; i++) {
-            _token.transfer(users[i], amounts[i]);
+            require(isCooldownPassed(users[i], tokenIds[i]), "Cooldown period not passed for user-token pair");
+            require(_token.transfer(users[i], amounts[i]), "Token transfer failed");
             lastIssuanceDate[users[i]][tokenIds[i]] = block.timestamp;
             emit TokensIssued(users[i], tokenIds[i], amounts[i]);
         }
-    }
-
-    /**
-     * @dev Allows the owner to deposit tokens to the contract.
-     * @param amount - Amount of tokens to deposit.
-     */
-    function depositTokens(uint256 amount) external onlyOwner {
-        require(_token.transferFrom(msg.sender, address(this), amount), "ERR06: Token transfer failed");
-        emit TokensDeposited(amount);
-    }
-
-    /**
-     * @dev Allows the owner to recover any ERC20 tokens sent to the contract by mistake.
-     * @param tokenAddress - Address of the ERC20 token to recover.
-     * @param recipient - Address where the recovered tokens will be sent.
-     * @param amount - Amount of tokens to recover.
-     */
-    function recoverERC20(address tokenAddress, address recipient, uint256 amount) external onlyOwner {
-        IERC20(tokenAddress).transfer(recipient, amount);
     }
 }
